04f4c98c09df1a1fdd33c172a0142aed6904d319
Gabriel Ventura (AQ3023672) <g.ventura@aluno.ifsp.edu.br>
2024-12-17 17:48:49 -0300

    Enviando o aplicativo sem o viewModel

---
+++ SitesFavoritos/app/src/main/java/br/edu/ifsp/dmo/sitesfavoritos/view/MainActivity.kt
@@ -0,0 +1,91 @@
+package br.edu.ifsp.dmo.sitesfavoritos.view
+
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.ifsp.dmo.sitesfavoritos.R
+import br.edu.ifsp.dmo.sitesfavoritos.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo.sitesfavoritos.databinding.AddsitesLayoutBinding
+import br.edu.ifsp.dmo.sitesfavoritos.model.Site
+import br.edu.ifsp.dmo.sitesfavoritos.view.adapters.SiteAdapter
+import br.edu.ifsp.dmo.sitesfavoritos.view.listeners.SiteItemClickListener
+
+class MainActivity : AppCompatActivity(), SiteItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private var dataSource = ArrayList<Site>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configListeners()
+        configRecyclerView()
+    }
+
+    override fun clickSiteItem(position: Int) {
+        val site = dataSource[position]
+        val mIntent  = Intent(Intent.ACTION_VIEW)
+
+        mIntent.setData(Uri.parse("http://" + site.url))
+        startActivity(mIntent)
+    }
+
+    override fun clickHeartSiteItem(position: Int) {
+        val site = dataSource[position]
+
+        site.favorito = !site.favorito
+        notifyAdapter()
+    }
+
+    private fun configListeners(){
+        binding.buttonAdd.setOnClickListener{handleAddSite()}
+    }
+
+    private fun configRecyclerView(){
+        val adapter = SiteAdapter(this, dataSource, this)
+
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+
+        binding.recyclerviewSites.layoutManager = layoutManager
+        binding.recyclerviewSites.adapter = adapter
+    }
+
+    private fun notifyAdapter(){
+        val adapter = binding.recyclerviewSites.adapter
+        adapter?.notifyDataSetChanged()
+    }
+
+    private fun handleAddSite(){
+        val tela = layoutInflater.inflate(R.layout.addsites_layout, null)
+        val bindingDialog: AddsitesLayoutBinding = AddsitesLayoutBinding.bind(tela)
+
+        val builder = AlertDialog.Builder(this)
+            .setView(tela)
+            .setTitle(R.string.novo_site)
+            .setPositiveButton(R.string.salvar,
+                DialogInterface.OnClickListener{dialog, which ->
+                    dataSource.add(
+                        Site(
+                            bindingDialog.edittextApelido.text.toString(),
+                            bindingDialog.edittextUrl.text.toString()
+                        )
+                    )
+                    notifyAdapter()
+                    dialog.dismiss()
+                })
+            .setNegativeButton(R.string.cancelar,
+                DialogInterface.OnClickListener{dialog, which ->
+                    dialog.dismiss()
+                })
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+}
---
+++ SitesFavoritos/app/src/main/java/br/edu/ifsp/dmo/sitesfavoritos/view/listeners/SiteItemClickListener.kt
@@ -0,0 +1,9 @@
+package br.edu.ifsp.dmo.sitesfavoritos.view.listeners
+
+interface SiteItemClickListener {
+
+    fun clickSiteItem(position: Int)
+
+    fun clickHeartSiteItem(position: Int)
+
+}
